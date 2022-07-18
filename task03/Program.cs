aec9cf6d51a554275957e9c4d7075b5cc3f661be
Rayenire <aeristarg@gmail.com>
2022-07-18 11:49:31 +0300

    Add task03

---
+++ task03/Program.cs
@@ -0,0 +1,48 @@
+﻿int[, ] CreateMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int x = new Random().Next(1, 1000);
+            matrix[i , j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+
+void AvgMatrix(int[, ] matrix)
+{
+    Console.WriteLine();
+    int[] array = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        int count = 1;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if(array[i] == array[j] && i != j)
+                count++;
+        }
+        Console.WriteLine(array[i] + " встречается " + count + " раз");
+    }
+
+}
