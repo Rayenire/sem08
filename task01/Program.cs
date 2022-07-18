33665395ed5459d10f96f940b8b9c3caa8cbae44
Rayenire <aeristarg@gmail.com>
2022-07-18 10:57:29 +0300

    Add task01

---
+++ task01/Program.cs
@@ -0,0 +1,49 @@
+﻿int[, ] CreateMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //int x = Convert.ToInt32(Console.ReadLine());
+            int x = new Random().Next(1, 1000);
+            matrix[i , j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+
+void AvgMatrix(int[, ] matrix)
+{
+    Console.WriteLine();
+    int x;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        x = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = x;
+    }
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[, ] matrix = new int[4, 4];
+matrix = CreateMatrix(matrix);
+AvgMatrix(matrix);
